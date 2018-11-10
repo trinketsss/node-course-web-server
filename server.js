666289a40e65b472af514acf20bbb71036cafa8e
Dev <dev@Devs-MBP.lan>
2018-11-10 22:15:53 +1100

    first commit

---
+++ server.js
@@ -0,0 +1,62 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+const app = express();
+
+hbs.registerPartials(__dirname+'/views/partials');
+app.set('view engine','hbs');
+
+//middleware
+app.use((req,res,next)=>{
+  var now = new Date().toString();
+  var log = `${now}:${req.method} ${req.url}`
+  console.log(log);
+  fs.appendFile('server.log',log+'\n',(err)=>{
+    if(err){
+      console.log(err);
+    }
+  })
+  next();
+});
+
+//middleware
+app.use((req,res,next)=>{
+  res.render('maintainence.hbs');
+})
+
+//middleware
+app.use(express.static(__dirname+"/public"));
+
+
+hbs.registerHelper('getCurrentYear',()=>{
+  return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt',(text)=>{
+  return text.toUpperCase();
+});
+
+app.get('/', (req,res)=> {
+  res.render('home.hbs',{
+    pageTitle:"Home page",
+    welcomeMessage:'Welcome back space cowboy'
+  })
+});
+
+app.get('/about',(req,res)=>{
+  res.render('about.hbs',{
+    pageTitle:"About page"
+  });
+});
+
+app.get("/bad",(req,res)=>{
+  res.send({
+    error:"not good",
+    type:"idunno"
+  });
+});
+
+app.listen(3000, ()=>{
+  console.log("server online");
+});
